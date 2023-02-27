9a18fceaa44af426a2a622a5eb82b2bbbf072e77
jiyul7 <jiyul7@daum.net>
2023-02-28 00:17:49 +0900

    more edit

--- Exercise/04_2_항목별 데이터 다듬기.R
+++ Exercise/04_2_항목별 데이터 다듬기.R
@@ -55,3 +55,38 @@ apt_price = merge(apt_price, loc, by ='code')
 # 주소 조합
 apt_price$jusoJibun = paste0(apt_price$addr_2, " ", apt_price$dongNm, " ", apt_price$jibun, " ", apt_price$aptNm)
 head(apt_price, 5)
+
+# 4단계 : 건축 연도, 전용면적 변환하기
+head(apt_price$conYear, 3)
+# 문자형 --> 숫자형
+# 건축연도
+#apt_price$conYear = apt_price$conYear %>% as.numeric()
+apt_price$conYear = as.numeric(apt_price$conYear)
+# 전용면적
+apt_price$area = apt_price$area %>% as.numeric() %>% round(0)
+head(apt_price$area, 3)
+
+# 5단계 : 평당 매매가
+# 평단가
+# py항목이 기정의되어 있지 않아도, 자동 생성됨
+# %>% 연산자 앞에는 ()로 끝나도록 묶어야 함
+apt_price$py = ((apt_price$price / apt_price$area) * 3.3) %>% round(0)
+head(apt_price, 3)
+# 6단계 : 층수 변환
+min(apt_price$floor)
+apt_price$floor = apt_price$floor %>% as.numeric() %>% abs()
+min(apt_price$floor)
+# 모든 행에 값이 1인 컬럼 추가 (거래 건수 파악이 목적)
+apt_price$cnt = 1
+head(apt_price, 3)
+
+# 04-3 전처리 데이터 저장하기
+
+# 1단계: 필요한 컬럼만 추출
+apt_price = apt_price %>% select(ymd, ym, year, code, addr_1, aptNm, jusoJibun, price, conYear, area, floor, py, cnt)
+head(apt_price, 3)
+# 2단계: 전처리 데이터 저장
+setwd(dirname(rstudioapi::getSourceEditorContext()$path)) 
+dir.create("./d04_pre_process")
+save(apt_price, file = "./d04_pre_process/04_pre_process.rdata")
+write.csv(apt_price, "./d04_pre_process/04_pre_process.csv")
